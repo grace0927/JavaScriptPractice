00d0b5e198bdd1f6b4e33e46b8b834d8df55faae
grace0927 <cellphone2008@gmail.com>
2016-01-14 17:12:53 -0500

    add options
    add options for text

--- jQueryPlugin/showMorePlugin.js
+++ jQueryPlugin/showMorePlugin.js
@@ -1,23 +1,28 @@
 ;(function($) {
-    $.fn.showMore = function(elem) {
-        this.each(function() {
+    $.fn.showMore = function(elem, options) {
+		var setting = $.extend({
+			more: '<span>Show More &#9660;</span>',
+		less: '<span>Show Less &#9650;</span>',
+		}, options);
+
+		this.each(function() {
         	elem.hide();
 			$(this).attr('data-now', '0');
-			$(this).html('').html('<span>Show More &#9660;</span>');
+			$(this).html('').html(setting.more);
         	$(this).unbind('click').bind('click', function(e) {
 				e.preventDefault();
 				var status = $(this).attr('data-now');
 	            if(status === '1') {
 					elem.hide();
 					$(this).attr('data-now', '0');
-					$(this).html('').html('<span>Show More &#9660;</span>');
+					$(this).html('').html(setting.more);
 			    } else {
 					elem.show();
 					$(this).attr('data-now', '1');
-					$(this).html('').html('<span>Show Less &#9650;</span>');
+					$(this).html('').html(setting.less);
 			    }
 			});
         });
         return this;
-    }; 
+    };
 }(jQuery));
