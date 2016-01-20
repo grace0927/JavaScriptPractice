fc3fb6fa5f9ee469e8d7f022aea60940247b8331
grace0927 <cellphone2008@gmail.com>
2016-01-20 16:27:16 -0500

    add one
    api switch plugin

---
+++ jQueryPlugin/apiSwitchPlugin.js
@@ -0,0 +1,42 @@
+;(function($) {
+	$.fn.apiSwitch = function(options) {
+		var setting = $.extend({
+			on: '<span>On</span>',
+			waiting: '<span>Processing</span>',
+			off: '<span>Off</span>',
+			api: '',
+			data: {},
+		}, options);
+
+		this.each(function() {
+			var status = $(this).attr('status');
+			
+			if(status === 'on') {
+				$(this).html('').html(setting.on);
+			} else {
+				$(this).html('').html(setting.off);
+			}
+			
+        	$(this).unbind('click').bind('click', function(e) {
+				e.preventDefault();
+				var status = $(this).attr('status');
+				var selector = $(this);
+				data = $.extend({
+					'status': status
+				}, setting.data);
+				$.ajax({
+					'url': setting.api,
+					'data': data,
+					'type': 'POST',
+					'dataType': 'json',
+				}).done(function(data) {
+					selector.attr('status', data.status);
+					content = (data.status=='on')?setting.on:setting.off;
+					selector.html('').html(content);
+				});
+				$(this).html('').html(setting.waiting);
+			});
+        });
+        return this;
+	};
+}(jQuery));
