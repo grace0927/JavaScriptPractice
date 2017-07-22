46da2221ef35538cf24098dad606d3c1e9860144
grace0927 <cellphone2008@gmail.com>
2017-07-22 08:01:22 -0400

    2017-07-22
    add docker support

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use( '/assets', routes.assets );
 app.use( '/showcase', routes.showcase );
 app.use( '/todo', routes.todo );
 
-app.listen( 8080, () => {} );
+app.listen( 80, () => {} );
 
 module.exports = app;
